aad3629d42e7afe7e6f34c9cbe35bacfec52a9dc
bruher <glushkovd2424@gmail.com>
2021-10-01 17:42:38 +0400

    files added

---
+++ racing/API/application/Application.php
@@ -0,0 +1,19 @@
+<?php
+	require_once('db/db.php');
+	require_once('users/Users.php');
+	class Application{
+		function __construct(){
+			$db = new DB();
+			$this->users = new Users($db);
+		}
+		public function login($params){
+			if($params['login'] && ['password']){
+				return $this->users->login($params ['login'], $params['password']);
+			}
+			$params['login'] = 'vasya';
+			$params['password'] = 'porol';
+			echo $params['login'];
+		}
+	}
+
+	?>
